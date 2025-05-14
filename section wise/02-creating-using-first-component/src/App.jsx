7444a863e45dd80299021b6ed790437cb7cba83b
radecoder <mohdsahildw@gmail.com>
2025-05-14 18:53:47 +0530

    props

--- section wise/02-creating-using-first-component/src/App.jsx
+++ section wise/02-creating-using-first-component/src/App.jsx
@@ -1,22 +1,57 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentImg from './assets/components.png'
+
+const reactDescriptions = ["Core", "Fundamental", "Essential"]
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header(){
+  const description = reactDescriptions[genRandomInt(2)];
   return(
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
 
+function CoreConcept(props){
+  return(<li>
+    <img src={props.img} alt="..."/>
+    <h3>{props.title} </h3>
+    <p> {props.description}</p>
+  </li>
+    
+
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header/>
+      <Header/> 
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title= "Components" description= "this is components description" img={componentImg}/>
+            <CoreConcept title= "Components" description= "this is components description" img={componentImg}/>
+            <CoreConcept title= "Components" description= "this is components description" img={componentImg}/>
+            <CoreConcept title= "Components" description= "this is components description" img={componentImg}/>
+        
+        
+          </ul>
+
+
+        </section>
+        
+
       </main>
     </div>
   );
